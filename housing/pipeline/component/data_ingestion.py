62bdb6e338ef60c7f0af956a25b6cab9fb413d5c
Abhishek Kumar Ojha <117336666+Abhishekojha2507@users.noreply.github.com>
2023-03-31 12:29:45 +0530

    added templates

---
+++ housing/pipeline/component/data_ingestion.py
@@ -0,0 +1,29 @@
+from housing.entity.config_entity import DataIngestionConfig
+import sys,os
+from housing.exception import HousingException
+from housing.logger import logging
+
+class DataIngestion:
+
+    def __init__(self,data_ingestion_config):
+        try:
+            logging.info(f"{'='*20}Data Ingestion log started.{'='*20}")
+            self.data_ingestion_config=data_ingestion_config
+        except Exception as e:
+            raise HousingException(e,sys)
+        
+    def initiate_data_ingestion(self)->DataIngestionArtifact:
+        try:
+            pass
+        except Exception as e:
+            raise HousingException(e,sys) from e
+
+
+
+
+
+
+
+
+
+
